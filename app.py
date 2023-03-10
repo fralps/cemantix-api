b294461f1b0f54916a8d478204d7345ed9ab940a
fralps <francois@kinoba.fr>
2023-03-10 16:54:39 +0100

    feat: add cemantle word to request response

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, jsonify
 import requests
-import json
 
 load_dotenv()
 
@@ -11,29 +10,37 @@ app = Flask(__name__)
 # ENV variables for Notion integration
 CEMANTIX_NOTION_TOKEN = os.getenv("CEMANTIX_NOTION_TOKEN")
 CEMANTIX_DATABASE_ID = os.getenv("CEMANTIX_DATABASE_ID")
+CEMANTLE_NOTION_TOKEN = os.getenv("CEMANTLE_NOTION_TOKEN")
+CEMANTLE_DATABASE_ID = os.getenv("CEMANTLE_DATABASE_ID")
 
-API_ENDPOINT = f"https://api.notion.com/v1/databases/{CEMANTIX_DATABASE_ID}/query"
 PAYLOAD = {"page_size": 500}
-HEADERS = {
-    "Authorization": f"Bearer {CEMANTIX_NOTION_TOKEN}",
-    "Content-Type": "application/json",
-    "Notion-Version": "2021-08-16",
-}
 
 @app.route('/')
 def get_stats():
     return jsonify(build_stats()), 200
 
 def build_stats():
-    cemantix_data = fetch_cemantix_data()
-    word_of_the_day = retrieve_word_of_the_day(cemantix_data)
+    cemantix_data = fetch_cemantix_data(CEMANTIX_NOTION_TOKEN, CEMANTIX_DATABASE_ID)
+    cemantle_data = fetch_cemantix_data(CEMANTLE_NOTION_TOKEN, CEMANTLE_DATABASE_ID)
 
     return {
-        'lastWord': word_of_the_day
+        "cemantix": {
+            "lastWord": retrieve_word_of_the_day(cemantix_data)
+        },
+        "cemantle": {
+            "lastWord": retrieve_word_of_the_day(cemantle_data)
+        }
     }
 
-def fetch_cemantix_data():
-    response = requests.post(API_ENDPOINT, json=PAYLOAD, headers=HEADERS)
+def fetch_cemantix_data(token, db_id):
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+        "Notion-Version": "2021-08-16",
+    }
+    api_endpoint = f"https://api.notion.com/v1/databases/{db_id}/query"
+
+    response = requests.post(api_endpoint, json=PAYLOAD, headers=headers)
 
     print(response.status_code, response.reason)
 
